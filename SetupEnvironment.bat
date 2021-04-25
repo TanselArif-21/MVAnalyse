786ff13eb5fb25d93e619e539c0a163d6d02e936
TanselArif-21 <tanselarif21@gmail.com>
2021-04-25 13:50:17 +0100

    Updating

--- SetupEnvironment.bat
+++ SetupEnvironment.bat
@@ -12,7 +12,25 @@ SET p_dir=%p_dir%activate
 rem activate the conda environment
 call %p_dir%
 
-if %errorlevel% neq 0 call C:\Users\%USERNAME%\AppData\Local\Continuum\anaconda3\Scripts\activate
+if %errorlevel% neq 0 (
+	echo Conda is not in path variable. Trying common location 1: C:\Users\%USERNAME%\AppData\Local\Continuum\anaconda3\Scripts\activate
+	call C:\Users\%USERNAME%\AppData\Local\Continuum\anaconda3\Scripts\activate
+) else (
+	echo Success! Found Activate file!
+)
+
+if %errorlevel% neq 0 (
+	echo Common location 1 not present. Trying common location 2: C:\Users\%USERNAME%\anaconda3\Scripts\activate
+	call C:\Users\%USERNAME%\anaconda3\Scripts\activate
+) else (
+	echo Success! Found Activate file!
+)
+
+if %errorlevel% neq 0 (
+	echo Could not find activate file in anaconda path
+) else (
+	echo Success! Found Activate file!
+)
 
 rem install virtualenv
 call pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org virtualenv
@@ -25,7 +43,10 @@ rem activate this new virtual environment
 call Code\venv\Scripts\activate
 
 rem if there is an error at this point, don't install anything
-if %errorlevel% neq 0 exit
+if %errorlevel% neq 0 (
+	echo There was an error activating virtual environment. Exiting without installing into venv
+	exit
+)
 
 rem install the accompanying requirements file
 call pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org -r Code\requirements.txt
